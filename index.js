ee99e90016209595bd3754c6c017183fd1f31744
Kayque Moraes <kaymoraesdias@gmail.com>
2023-04-24 11:17:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const empresa = document.querySelector('#empresa').value;
+  if (empresa === 'CDSP-DISTRIBUIDOR') {
+    Swal.fire({
+      title: 'Sucesso!',
+      text: 'Você será redirecionado para a página do distribuidor.',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    }).then(() => {
+      window.location.href = 'distribuidor.html';
+    });
+  } else {
+    Swal.fire({
+      title: 'Erro!',
+      text: 'O nome da empresa não é válido.',
+      icon: 'error',
+      confirmButtonText: 'OK'
+    });
+  }
+});
